eced87329365843a18f02d6771b0fdddb9b67f84
Saya <107242326+sayasingh@users.noreply.github.com>
2025-09-19 15:16:23 +0545

    useRef Hook in react

---
+++ useRef/src/MyComponent.jsx
@@ -0,0 +1,56 @@
+import React, {useState, useEffect, useRef} from "react";
+
+function MyComponent(){
+
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+
+    useEffect(() => {
+        console.log("COMPONENT RENDERED");
+    });
+
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor = "pink";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "pink";
+        inputRef3.current.style.backgroundColor = "";
+    }
+
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef1.current.style.backgroundColor = "";
+        inputRef2.current.style.backgroundColor = "";
+        inputRef3.current.style.backgroundColor = "pink";
+    }
+
+    return(
+        <div>
+            <button onClick={handleClick1}>
+        Click Me 1!
+    </button>
+    <input ref={inputRef1}/>
+
+    <button onClick={handleClick2}>
+        Click Me 2!
+    </button>
+    <input ref={inputRef2}/>
+
+    <button onClick={handleClick3}>
+        Click Me 3!
+    </button>
+    <input ref={inputRef3}/>
+
+        </div>
+
+    );
+ 
+}
+export default MyComponent;
